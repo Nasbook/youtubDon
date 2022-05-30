a130aece0775079d8ce09b8c09426a6fe4798ea8
ADham <cood_ip@hotmail.com>
2022-05-30 20:03:01 +0200

    Update down.js
    update this code 2022

--- down.js
+++ down.js
@@ -1,7 +1,3 @@
-
-var CourseTitle=document.querySelectorAll("#title a"),
-x=document.querySelectorAll("a#thumbnail.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail"),contener="\n\n\n",dev="\n DEVeloper ADham allam .. \n Cood_ip{}hotmail.com";
-for (var i = x.length - 1; i >= 0; i--) {
-var obj =  x[i].getAttribute("href").slice(9,20);
-contener +="youtube-dl https://www.youtube.com/watch?v="+obj+"\n"; };
-console.log("\n\n"+CourseTitle[0].innerText+"\nTotal Videos [ "+x.length+" ]"+dev+contener);
+var x=document.querySelectorAll(".yt-simple-endpoint.style-scope.ytd-playlist-video-renderer"),contener="\n\n\n",contener="\n\n\n";
+for (var i = x.length - 1; i >= 0; i--) {  var hreftxs = x[i].getAttribute("href").split("v=")[1].substring(0, 11);
+contener +="youtube-dl https://www.youtube.com/watch?v="+hreftxs+"\n"; };  console.log(contener);
